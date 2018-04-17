1fbf1adc41fa81340b0cd79b6a04941b4cfaf074
Joris <joris.van.delft@ordina.nl>
2018-04-17 09:51:13 +0200

    added comment for testing hook

--- src/test/java/steps/ExampleSteps.java
+++ src/test/java/steps/ExampleSteps.java
@@ -11,7 +11,7 @@ import Impl.BrowserHelper;
 
 public class ExampleSteps {
 
-    //Implementatie implementatie aaaaa;
+    //Implementatie implementatie aaaaaaaa;
     Implementatie implementatie = new Implementatie();
 
     @Given("^I am on the homepage and click on cars$")
